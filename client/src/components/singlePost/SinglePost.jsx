cd5d402a4a75760fe2f42250621bfe770fd0937b
Raouf Satto <raoufsato999@gmail,com>
2022-01-06 18:20:21 +0300

    post updated work

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,23 +1,28 @@
-import React, { useEffect, useState, useContext } from "react";
-import { Link, useLocation } from "react-router-dom";
 import axios from "axios";
-
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 import "./singlePost.css";
-import { Context } from "./../../context/Context";
 
 export default function SinglePost() {
-	const { user } = useContext(Context);
-	const PF = "http://localhost:5000/images/";
-	const [post, setPost] = useState({});
 	const location = useLocation();
 	const path = location.pathname.split("/")[2];
+	const [post, setPost] = useState({});
+	const PF = "http://localhost:5000/images/";
+	const { user } = useContext(Context);
+	const [title, setTitle] = useState("");
+	const [desc, setDesc] = useState("");
+	const [updateMode, setUpdateMode] = useState(false);
 
 	useEffect(() => {
-		const fetchPost = async () => {
-			const res = await axios.get(`/posts/${path}`);
+		const getPost = async () => {
+			const res = await axios.get("/posts/" + path);
 			setPost(res.data);
+			setTitle(res.data.title);
+			setDesc(res.data.desc);
 		};
-		fetchPost();
+		getPost();
 	}, [path]);
 
 	const handleDelete = async () => {
@@ -26,39 +31,76 @@ export default function SinglePost() {
 				data: { username: user.username },
 			});
 			window.location.replace("/");
-		} catch (error) {}
+		} catch (err) {}
 	};
+
+	const handleUpdate = async () => {
+		try {
+			await axios.put(`/posts/${post._id}`, {
+				username: user.username,
+				title,
+				desc,
+			});
+			setUpdateMode(false);
+		} catch (err) {}
+	};
+
 	return (
 		<div className="singlePost">
 			<div className="singlePostWrapper">
 				{post.photo && (
-					<img className="singlePostImg" src={PF + post.photo} alt="" />
+					<img src={PF + post.photo} alt="" className="singlePostImg" />
 				)}
-
-				<h1 className="singlePostTitle">
-					{post.title}
-					{post.username === user?.username && (
-						<div className="singlePostEdit">
-							<i className="singlePostIcon far fa-edit"></i>
-							<i
-								className="singlePostIcon far fa-trash-alt"
-								onClick={handleDelete}
-							></i>
-						</div>
-					)}
-				</h1>
+				{updateMode ? (
+					<input
+						type="text"
+						value={title}
+						className="singlePostTitleInput"
+						autoFocus
+						onChange={(e) => setTitle(e.target.value)}
+					/>
+				) : (
+					<h1 className="singlePostTitle">
+						{title}
+						{post.username === user?.username && (
+							<div className="singlePostEdit">
+								<i
+									className="singlePostIcon far fa-edit"
+									onClick={() => setUpdateMode(true)}
+								></i>
+								<i
+									className="singlePostIcon far fa-trash-alt"
+									onClick={handleDelete}
+								></i>
+							</div>
+						)}
+					</h1>
+				)}
 				<div className="singlePostInfo">
-					<span>
+					<span className="singlePostAuthor">
 						Author:
-						<b className="singlePostAuthor">
-							<Link className="link" to={`/posts?user=${post.username}`}>
-								{post.username}
-							</Link>
-						</b>
+						<Link to={`/?user=${post.username}`} className="link">
+							<b> {post.username}</b>
+						</Link>
 					</span>
-					<span>{new Date(post.createdAt).toDateString()}</span>
+					<span className="singlePostDate">
+						{new Date(post.createdAt).toDateString()}
+					</span>
 				</div>
-				<p className="singlePostDesc">{post.desc}</p>
+				{updateMode ? (
+					<textarea
+						className="singlePostDescInput"
+						value={desc}
+						onChange={(e) => setDesc(e.target.value)}
+					/>
+				) : (
+					<p className="singlePostDesc">{desc}</p>
+				)}
+				{updateMode && (
+					<button className="singlePostButton" onClick={handleUpdate}>
+						Update
+					</button>
+				)}
 			</div>
 		</div>
 	);
